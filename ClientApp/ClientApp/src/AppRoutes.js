fd8288731a63c89cbb99266be4557638e4b9a323
rajini <k.rajini1992@gmail.com>
2024-02-14 19:05:32 +0000

    Commit changes for unneccessary files

--- ClientApp/ClientApp/src/AppRoutes.js
+++ ClientApp/ClientApp/src/AppRoutes.js
@@ -1,5 +1,4 @@
-import { Counter } from "./components/Counter";
-import { FetchData } from "./components/FetchData";
+
 import { Home } from "./components/Home";
 import NewClient from "./components/NewClient";
 import UpdateClient from "./components/UpdateClient";
@@ -9,14 +8,7 @@ const AppRoutes = [
     index: true,
     element: <Home />
   },
-  {
-    path: '/counter',
-    element: <Counter />
-  },
-  {
-    path: '/fetch-data',
-    element: <FetchData />
-    },
+  
     {
         path: '/new-client/',
         element: <NewClient />
